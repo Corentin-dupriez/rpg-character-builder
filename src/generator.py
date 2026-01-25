d7a0bd3ecc8e4dbfb4b410370a71ff10f64b16bf
Corentin Dupriez <dupriez.c@icloud.com>
2026-01-25 16:42:30 +0200

    Added class generator

--- src/generator.py
+++ src/generator.py
@@ -1,18 +1,33 @@
 import os
 import random
 import json
-from models import RaceData, Character
+from typing import Dict
+from models import RaceData, Character, ClassesData, Skills
 
 data_folder = os.path.join(
     os.path.abspath(os.path.join(os.getcwd(), os.pardir)), "data"
 )
 
 
-def load_races() -> RaceData:
-    with open(os.path.join(data_folder, "races.json")) as file:
-        data = json.load(file)
+def open_file(file_name) -> Dict:
+    with open(os.path.join(data_folder, file_name)) as f:
+        data = json.load(f)
 
+    return data
+
+
+def load_races() -> RaceData:
+    data = open_file("races.json")
     return RaceData.model_validate(data)
+
+
+def load_classes() -> ClassesData:
+    data = open_file("classes.json")
+    return ClassesData.model_validate(data)
+
+
+def load_skills() -> Skills:
+    pass
 
 
 def choose_race() -> str:
@@ -24,8 +39,21 @@ def choose_race() -> str:
     return random.choices(names, weights=weights, k=1)[0]
 
 
+def choose_class() -> str:
+    classes = {
+        class_data.name: class_data.weight for class_data in load_classes().classes
+    }
+
+    classes_names = list(classes.keys())
+    weights = list(classes.values())
+
+    return random.choices(classes_names, weights=weights, k=1)[0]
+
+
 def build_character() -> Character:
-    return Character(race=choose_race(), character_class="", skills=[], description="")
+    return Character(
+        race=choose_race(), character_class=choose_class(), skills=[], description=""
+    )
 
 
 created_character = build_character()
