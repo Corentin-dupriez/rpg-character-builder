5ee5e3e169b1291fada08b7f6298d94ffa98fa6b
Corentin Dupriez <dupriez.c@icloud.com>
2026-02-05 22:47:16 +0200

    Improved documentation

--- src/generator.py
+++ src/generator.py
@@ -18,30 +18,81 @@ data_folder = BASE_DIR / "data"
 
 
 def open_file(file_name: str) -> Dict:
+    """
+    Opens a json file from the data folder based on the provided file name.
+    Args:
+        file_name: the name of the file to open
+    Returns:
+        A dictionary representing the json object
+    Raises:
+        FileNotFoundError: if the file doesn't exist
+        json.JSONDecodeError: if the file is not a valid JSON
+    """
     with open(data_folder / file_name, encoding="utf-8") as f:
         return json.load(f)
 
 
 def load_races() -> RaceData:
+    """
+    Load and validates the race data from the races json file.
+    Returns:
+        RaceData: an object containing the several Race objects
+    Raises:
+        pydantic.ValidationError: If the JSON data does not conform
+            to the `RaceData` schema.
+        FileNotFoundError: If the races JSON file cannot be found.
+        json.JSONDecodeError: If the file is not valid JSON.
+    """
     data = open_file("races.json")
     return RaceData.model_validate(data)
 
 
 def load_classes() -> ClassesData:
+    """
+    Load and validates the classes data from the classes json file.
+    Returns:
+        ClassesData: an object containing several ClassInfo objects
+    Raises:
+        pydantic.ValidationError: If the JSON data doesn't conform
+            to the `ClassesData` schema.
+        FileNotFoundError: If the classes JSON file cannot be found.
+        json.JSONDecodeError: If the file is not valid JSON.
+    """
     data = open_file("classes.json")
     return ClassesData.model_validate(data)
 
 
 def load_skills() -> Skills:
+    """
+    Load and validates the skills data from the skills json file.
+    Returns:
+        Skills: an object containing a list of skills
+    Raises:
+        pydantic.ValidationError: If the JSON data doesn't conform
+            to the `Skills` schema.
+        FileNotFoundError: If the classes JSON file cannot be found.
+        json.JSONDecodeError: If the file is not valid JSON.
+    """
     data = open_file("skills.json")
     return Skills.model_validate(data)
 
 
 def load_backgrounds() -> BackgroundsData:
+    """
+    Load and validates the backgrounds data from the backgrounds json file.
+    Returns:
+        BackgroundsData: an object containing several backgrounds objects
+    Raises:
+        pydantic.ValidationError: If the JSON data doesn't conform
+            to the `BackgroundsData` schema.
+        FileNotFoundError: If the classes JSON file cannot be found.
+        json.JSONDecodeError: If the file is not valid JSON.
+    """
     data = open_file("backgrounds.json")
     return BackgroundsData.model_validate(data)
 
 
+# Instantiate the data from the JSON files to keep in memory
 _RACES = load_races()
 _CLASSES = load_classes()
 _SKILLS = load_skills()
@@ -49,15 +100,30 @@ _BACKGROUNDS = load_backgrounds()
 
 
 def choose_race() -> Race:
+    """
+    Generates a random choice of race.
+    The likelihood of a race being picked depends on the specific weights of each race.
+    Returns:
+        Race: a randomly selected `Race` instance
+    """
     return random.choices(_RACES.races, weights=[r.weight for r in _RACES.races], k=1)[
         0
     ]
 
 
 def choose_class(race: Race) -> ClassInfo:
+    """
+    Generates a random choice of class.
+    The likelihood of a class being picked depends on the class affinities of the race passed as input
+    Args:
+        race: a Race object representing the race used to generate the class. This will be used to calculate the weights for chosing the class.
+    Returns:
+        ClassInfo: a randomly selected `ClassInfo` instance
+    """
     affinities = race.class_affinities
     classes = []
     weights = []
+
     for c in _CLASSES.classes:
         modifier = affinities.get(c.name, 1.0)
         classes.append(c)
